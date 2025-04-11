a38a954d541b848346e33eb114ec35da28d2541e
kifayatmuaz <kifayatmuaz@gmail.com>
2025-04-12 04:05:09 +0600

    a

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html", // Path to your HTML files
+      "./src/**/*.{js,jsx,ts,tsx}", // Path to your JS/TS files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
